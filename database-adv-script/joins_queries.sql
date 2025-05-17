5bb81c1ff7b9a5d98e1ea4e2cac439ea97f964d2
jackipbore <jackipbore@gmail.com>
2025-05-17 21:06:39 +0300

    Update joins_queries.sql

--- database-adv-script/joins_queries.sql
+++ database-adv-script/joins_queries.sql
@@ -9,3 +9,14 @@ SELECT p.id AS property_id, p.name AS property_name, p.location,
        r.id AS review_id, r.rating, r.comment
 FROM properties p
 LEFT JOIN reviews r ON p.id = r.property_id;
+
+-- FULL OUTER JOIN simulation: Users and Bookings
+SELECT u.id AS user_id, u.name AS user_name, b.id AS booking_id, b.property_id
+FROM users u
+LEFT JOIN bookings b ON u.id = b.user_id
+
+UNION
+
+SELECT u.id AS user_id, u.name AS user_name, b.id AS booking_id, b.property_id
+FROM users u
+RIGHT JOIN bookings b ON u.id = b.user_id;
