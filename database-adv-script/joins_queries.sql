748ac1d5e034b161fc283c7f37663ff776268cb8
jackipbore <jackipbore@gmail.com>
2025-05-17 21:06:05 +0300

    Update joins_queries.sql

--- database-adv-script/joins_queries.sql
+++ database-adv-script/joins_queries.sql
@@ -4,3 +4,8 @@ SELECT b.id AS booking_id, b.property_id, b.start_date, b.end_date,
 FROM bookings b
 INNER JOIN users u ON b.user_id = u.id;
 
+-- LEFT JOIN: Properties and their reviews
+SELECT p.id AS property_id, p.name AS property_name, p.location,
+       r.id AS review_id, r.rating, r.comment
+FROM properties p
+LEFT JOIN reviews r ON p.id = r.property_id;
