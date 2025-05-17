dea42195400d7fde1b94b17a11ceab89903d8ae1
jackipbore <jackipbore@gmail.com>
2025-05-17 20:51:28 +0300

    Update joins_queries.sql

--- database-adv-script/joins_queries.sql
+++ database-adv-script/joins_queries.sql
@@ -1,1 +1,6 @@
+-- INNER JOIN: Bookings with Users
+SELECT b.id AS booking_id, b.property_id, b.start_date, b.end_date,
+       u.id AS user_id, u.name AS user_name, u.email
+FROM bookings b
+INNER JOIN users u ON b.user_id = u.id;
 
